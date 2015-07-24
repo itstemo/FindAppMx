1d1c4b322a4854e2951f0280d386399ba6407c5f
itstemo <ojeda.temo@gmail.com>
2015-07-24 11:27:43 -0500

    Arrival error en parse de Encontrados, junta las 2 bases en el mapa principal.

--- public/mapsparse.js
+++ public/mapsparse.js
@@ -14,6 +14,9 @@ $(document).ready(function() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 	Parse.initialize("LDUjtHoZzgepi460STD39zOo2ah6i5SEPD8OsGpH", "b1ayhsBbaERGv2pp50iqXHmFUEpeQFyt7pXxLwYV");
+		
+	var Encontrado = Parse.Object.extend("Encontrados");
+	var query1 = new Parse.Query(Encontrado);	
 
 	var Perdido = Parse.Object.extend("Perdidos");
 	var query = new Parse.Query(Perdido);
@@ -27,19 +30,13 @@ google.maps.event.addDomListener(window, 'load', initialize);
 		var lon = Number(object.get("longitud"));
 		  var imagenFile = object.get('imagen');
 			var imagenURL = imagenFile.url();
+			var caract = object.get("caracteristicas");
 
 		 var latlng = new google.maps.LatLng(lat,lon);
 		var nombre = object.get("nombre");
-<<<<<<< Updated upstream:public/mapsparse.js
-
-		var myinfowindow = new google.maps.InfoWindow({
-			content: '<b>' + nombre + '</b> <img src = '+imagenURL+' width="200" height="200">'
-=======
-		var caract = object.get("caracteristicas");
 		
 		var myinfowindow = new google.maps.InfoWindow({
 			content: '<b>' + nombre + '</b> <img src = '+imagenURL+' width="200" height="200"> </br><p>' + caract + '</p>'  
->>>>>>> Stashed changes:mapsparse.js
 		});
 
 		var marker = new google.maps.Marker({
@@ -57,8 +54,45 @@ google.maps.event.addDomListener(window, 'load', initialize);
   error: function(error) {
     alert("Error: " + error.code + " " + error.message);
   }
-});
+})
+			  
+			  
+query1.find({
+  success: function(results) {
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) {
+      var object = results[i];
+		var lat = Number(object.get("latitud"));
+		var lon = Number(object.get("longitud"));
+		  var imagenFile = object.get('imagen');
+			var imagenURL = imagenFile.url();
+			var caract = object.get("caracteristicas");
 
-	}
+		 var latlng = new google.maps.LatLng(lat,lon);
+		var nombre = object.get("nombre");
+		
+		var myinfowindow = new google.maps.InfoWindow({
+			content: '<b>' + nombre + '</b> <img src = '+imagenURL+' width="200" height="200"> </br><p>' + caract + '</p>'  
+		});
 
+		var marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+				icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 
+			 infowindow: myinfowindow
+
+              });
+		google.maps.event.addListener(marker, 'click', function() {
+    this.infowindow.open(map,this);
+  });
+
+    }
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+})
+	
+
+}
 });
